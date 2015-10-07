c58c1d6a742968668616a9f25e3a6043b2570438
Ryan Haase <ryandhaase@gmail.com>
2015-10-07 11:26:10 -0700

    Seeded database with reviews and restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+require 'faker'
+
+# sample_image = Restaurant.find(2)
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# 10.times do
+#   Restaurant.create!(
+#     name: Faker::Company.name,
+#     address: "#{Faker::Address.street_address}, #{Faker::Address.city}, #{Faker::Address.state_abbr} #{Faker::Number.number(5)}",
+#     phone: '555-555-5555',
+#     website: Faker::Internet.url,
+#     image: sample_image.image
+#   )
+# end
+
+restaurants = Restaurant.all
+
+50.times do
+  Review.create!(
+    restaurant_id: restaurants.sample.id,
+    rating: Faker::Number.between(1, 5),
+    comment: Faker::Lorem.paragraph
+  )
+end
+
+puts "Seed Finished"
